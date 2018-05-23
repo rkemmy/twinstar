869bb4327c5ffb3bc7dd65b333e81243776ba1c7
rkemmy <rkemmy.rk69@gmail.com>
2018-05-23 16:52:46 +0300

    create url for editing profile

--- twinstar/funstar/urls.py
+++ twinstar/funstar/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.index,name='index'),
     url(r'^accounts/profile/$', views.profile, name='profile'),
+    url(r'^accounts/profile/edit/$',views.edit_profile,name='edit_profile'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
