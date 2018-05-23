73ec5d9396674e0758e1e4720ec31393320c7c16
rkemmy <rkemmy.rk69@gmail.com>
2018-05-23 16:37:37 +0300

    create a view function for the profile

--- twinstar/funstar/views.py
+++ twinstar/funstar/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Image, Profile
 
 # Create your views here.
 #@login_required(login_url='/accounts/login/')
 def index(request):
-    return render(request, 'index.html')
+    pics = Image.get_all()
+    return render(request, 'index.html', {"pics":pics})
 
 def signup(request):
     return render(request, 'signup.html')
+
+def profile(request):
+    images = Image.objects.filter(owner_id=request.user)
+    profiles = Profile.objects.filter(user=request.user)
+    current_user = request.user
+
+    return render(request, 'profile.html',{"current_user": current_user,"images": images,"profiles": profiles})
