4cdfa7d2e725c4548b59cb5df5a62432937e6cbb
rkemmy <rkemmy.rk69@gmail.com>
2018-05-24 20:11:42 +0300

    create function for posting images

--- twinstar/funstar/views.py
+++ twinstar/funstar/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Image, Profile
-from .forms import ProfileForm
+from .forms import ProfileForm, ImageUpload
 
 # Create your views here.
-#@login_required(login_url='/accounts/login/')
+@login_required(login_url='/accounts/login/')
 def index(request):
     pics = Image.get_all()
     return render(request, 'index.html', {"pics":pics})
@@ -40,3 +40,17 @@ def explore(request):
     image = Image.objects.all()
 
     return render(request, 'explore.html', {"image": image})
+
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageUpload(request.POST, request.FILES)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.user = current_user
+            photo.save()
+            return redirect('index')
+    else:
+        form = ImageUpload()
+    return render(request, 'new_post.html', {"form": form})
+
