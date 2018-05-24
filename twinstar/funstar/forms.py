736067737be88304f90d52559cffa04e50cbdd25
rkemmy <rkemmy.rk69@gmail.com>
2018-05-24 20:10:46 +0300

    create form for uploading images

--- twinstar/funstar/forms.py
+++ twinstar/funstar/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Profile
+from .models import Profile, Image
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ['date','user_name','user']
+
+class ImageUpload(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['post_date', 'owner', 'profile']
