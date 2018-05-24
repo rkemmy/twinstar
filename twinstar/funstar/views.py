858d9116c9eb3b914f9be7d8cb1d4a2c59c54a31
rkemmy <rkemmy.rk69@gmail.com>
2018-05-24 14:56:35 +0300

    enable the edit profile function to save the new data

--- twinstar/funstar/views.py
+++ twinstar/funstar/views.py
@@ -16,20 +16,27 @@ def profile(request):
     images = Image.objects.filter(owner_id=request.user)
     profiles = Profile.objects.filter(user=request.user)
     current_user = request.user
+    form = ProfileForm()
+    avatar = Profile.avatar
 
-    return render(request, 'profile.html',{"current_user": current_user,"images": images,"profiles": profiles})
+    return render(request, 'profile.html',{"current_user": current_user,"images": images,"profiles": profiles, "avatar":avatar, "form":format})
 
 def edit_profile(request):
     current_user = request.user
     user_profile = Profile.objects.filter(user_id=current_user)
     if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES)
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
         if form.is_valid():
-            user_profile = form.save(commit=False)
+            user_profile = form.save()
             user_profile.user = current_user
             user_profile.save()
-            return redirect('index')
+            return redirect('profile')
     else:
         form = ProfileForm()
 
     return render(request, 'edit-profile.html', {"form":form})
+
+def explore(request):
+    image = Image.objects.all()
+
+    return render(request, 'explore.html', {"image": image})
