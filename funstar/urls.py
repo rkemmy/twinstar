89b0cbc50acc2d956bb104fe2f292af354bf42e5
rkemmy <rkemmy.rk69@gmail.com>
2018-05-30 16:16:47 +0300

    add comment url

--- funstar/urls.py
+++ funstar/urls.py
@@ -8,6 +8,7 @@ urlpatterns=[
     url(r'^accounts/profile/$', views.profile, name='profile'),
     url(r'^accounts/profile/edit/$',views.edit_profile,name='edit_profile'),
     url(r'^post/new/$', views.new_post, name='new_post'),
+    url(r'^add/(\d+)', views.comment, name='comment'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
