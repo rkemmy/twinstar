1939207b38a4f928f0cd9c4e9fe9857fae311cc3
rkemmy <rkemmy.rk69@gmail.com>
2018-05-30 16:50:08 +0300

    make few changes

--- funstar/views.py
+++ funstar/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Image, Profile, Comment
 from .forms import ProfileForm, ImageUpload, CommentForm
 
